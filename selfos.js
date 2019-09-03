d7513226b2d767dbf3831b709f295614b2ed5693
salazarhugo <54861492+salazarhugo@users.noreply.github.com>
2019-09-03 20:33:39 +0200

    Add files via upload

---
+++ selfos.js
@@ -0,0 +1,50 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const bot_name = "gervais";
+
+client.on("ready", () => {
+    const channel = client.channels.get("557872267258757131");
+    if (!channel) return console.error("The channel does not exist!");
+    channel.join().then(connection => {
+      // Yay, it worked!
+      console.log("Successfully connected.");
+    }).catch(e => {
+      // Oh no, it errored! Let's log it to console :)
+      console.error(e);
+    });
+  });
+
+  client.on("message", async message => {
+
+    if(message.author.bot) return;
+
+    if(message.content.startsWith("!" + bot_name)) {
+        const args = message.content.slice(bot_name.length + 1).trim().split(/ +/g);
+        const command = args.shift().toLowerCase();
+
+        if(command === "say") {
+          const sayMessage = args.join(" ");
+          message.delete().catch(O_o=>{});
+          message.channel.send(sayMessage);
+        } else
+        if(command === "activity") {
+          const sayMessage = args.join(" ");
+          message.delete().catch(O_o=>{});
+          client.user.setActivity(sayMessage);
+        }
+    } else
+    if(message.content.startsWith("!")) {
+        const args = message.content.slice(1).trim().split(/ +/g);
+        const command = args.shift().toLowerCase();
+
+        if(command === "say") {
+          const sayMessage = args.join(" ");
+          message.delete().catch(O_o=>{});
+          message.channel.send(sayMessage);
+        }
+}
+
+
+  });
+
+client.login(process.env.BOT_TOKEN);
