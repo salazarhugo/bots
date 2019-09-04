f94aefbd0ebe993c4061124d041577b22f8ba8b3
salazarhugo <54861492+salazarhugo@users.noreply.github.com>
2019-09-04 19:47:48 +0200

    Update epita-bot.js

--- epita-bot.js
+++ epita-bot.js
@@ -39,7 +39,8 @@ client.on("ready", () => {
         const command = args.shift().toUpperCase();
         console.log(command);
         if(command === "A1" || command === "A2" || command === "B1" || command === "B2" || command === "C1" || command === "C2" || command === "D1" || command === "D2" || command === "ENG1" || command === "ENG2" || command === "ENG3") { 
-            message.member.removeRoles(message.guild.roles.find(r => r.name === A1), message.guild.roles.find(r => r.name === A2), message.guild.roles.find(r => r.name === B1), message.guild.roles.find(r => r.name === B2), message.guild.roles.find(r => r.name === C1), message.guild.roles.find(r => r.name === C2), message.guild.roles.find(r => r.name === D1), message.guild.roles.find(r => r.name === D2), message.guild.roles.find(r => r.name === ENG1), message.guild.roles.find(r => r.name === ENG2));
+           
+            message.member.removeRoles(message.member.roles);
             message.member.addRole(message.guild.roles.find(r => r.name === command)).catch(console.error);
         } else if(command === "ADMIN") {
             message.channel.send("Nope bitch!");
